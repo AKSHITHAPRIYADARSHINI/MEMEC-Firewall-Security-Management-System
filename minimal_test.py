bc74a53af490d29ba17bc08bf7a4cda47a5651c6
Akshitha <akshi@example.com>
2026-01-20 23:21:40 -0600

    Replace repository with new project structure

--- minimal_test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/test')
-def test():
-    return 'OK - Server works!'
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5001, debug=False)
